126a181aa17f92e9095bf9053f105551e789a2cf
Debraj Dhar <debrajdhar100@gmail.com>
2023-10-14 00:29:23 +0530

    added product details

---
+++ client/src/Components/ProductCard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProductCard = () => {
+  return <div className="my-2 py-2 px-1">ProductCard</div>;
+};
+
+export default ProductCard;
--- client/src/Components/ProductListing.js
+++ client/src/Components/ProductListing.js
@@ -1,4 +1,6 @@
 import React from "react";
+import data from "../data/testData.js";
+import ProductCard from "./ProductCard.js";
 
 const ProductListing = () => {
   return (
@@ -12,6 +14,18 @@ const ProductListing = () => {
           <button>
             <i className="fa-solid fa-magnifying-glass absolute right-0 top-0 bottom-0 text-gray-600 font-bold px-3 m-auto h-fit"></i>
           </button>
+        </div>
+      </div>
+      <div className="p-4 flex">
+        <div className="w-2/12 border-r-2">Filters</div>
+        <div className="products w-10/12 pl-4 pr-2">
+          {data.length > 0 ? (
+            data.map((product, idx) => (
+              <ProductCard key={idx} product={product} />
+            ))
+          ) : (
+            <h1>No data found.</h1>
+          )}
         </div>
       </div>
     </div>
