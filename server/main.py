90a9e36c141402ae8100afc3da6661a0df339298
Debraj Dhar <debrajdhar100@gmail.com>
2023-10-13 23:59:59 +0530

    added server files

--- server/main.py
+++ server/main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
+import uvicorn
+import pandas as pd
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"hello"}
+@app.get("/read-csv")
+def read_csv():
+    try:
+        csv_file_path = "../server/dataset/archive/air conditioners.csv"
+        df = pd.read_csv(csv_file_path)
+        print("---------", df)
+        data = df.to_dict(orient="records")
+        print("---------", data)
+        return data
+
+    except Exception as e:
+        return {"error": str(e)}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
