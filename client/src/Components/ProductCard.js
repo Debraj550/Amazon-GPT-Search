bb410ae953f2b888d26483ef72be9627314f9561
Debraj Dhar <debrajdhar100@gmail.com>
2023-10-14 01:06:41 +0530

    added fake product data

--- client/src/Components/ProductCard.js
+++ client/src/Components/ProductCard.js
@@ -1,7 +1,22 @@
 import React from "react";
 
-const ProductCard = () => {
-  return <div className="my-2 py-2 px-1">ProductCard</div>;
+const ProductCard = (props) => {
+  const { product } = props;
+  console.log(product);
+  return (
+    <div className="mb-6 mt-2 py-2 px-1 flex overflow-hidden h-40 items-center bg-gray-100">
+      <div className="image w-3/12">
+        <img className="" src={product.image} alt={`${product.name}`}></img>
+      </div>
+      <div className="details w-9/12 ml-4 mr-2 px-2 h-full mt-6">
+        <h1 className="font-semibold ">{product.name}</h1>
+        <div className="flex text-sm">
+          <p>{product.ratings ? product.ratings : 0}/5</p>
+          <p>{product.no_of_ratings}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductCard;
