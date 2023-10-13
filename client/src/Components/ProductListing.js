2959169aa957608e150084594422486e998a8685
Debraj Dhar <debrajdhar100@gmail.com>
2023-10-13 21:16:03 +0530

    added search bar button

--- client/src/Components/ProductListing.js
+++ client/src/Components/ProductListing.js
@@ -3,13 +3,15 @@ import React from "react";
 const ProductListing = () => {
   return (
     <div className="p-4">
-      <div className="search-bar m-auto w-4/12 relative">
-        <div className="w-full relative">
+      <div className="search-bar m-auto w-4/12 relative ">
+        <div className="w-full">
           <input
             className="p-2 border-2 w-full rounded-lg"
             placeholder="Search"
           ></input>
-          <i className="fa-solid fa-magnifying-glass absolute translate-y-["></i>
+          <button>
+            <i className="fa-solid fa-magnifying-glass absolute right-0 top-0 bottom-0 text-gray-600 font-bold px-3 m-auto h-fit"></i>
+          </button>
         </div>
       </div>
     </div>
